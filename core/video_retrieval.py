c1ae50b1c79b9c6f6570119232f5ffba7c24383b
AymanNasser <aymannasr43@gmail.com>
2021-05-29 00:08:27 +0200

    type: feat 1. Added match function to video_ret 2. Added feature_extr based on histograms for keyframes
    Signed-off-by: AymanNasser <aymannasr43@gmail.com>

--- core/video_retrieval.py
+++ core/video_retrieval.py
@@ -3,14 +3,16 @@ import cv2
 from numpy.core.fromnumeric import std
 from feature_handler import FeatureHandler
 import matplotlib.pyplot as plt
+from matching_fns import *
 
 
 class KeyframeExtraction(FeatureHandler):
-    def __init__(self, k1 = 1, k2 = 1.5):
+    def __init__(self, k1 = 1, k2 = 1.5, matching_threshold=150):
         super().__init__()
         self.threshold_value = None
         self.k1 = k1
         self.k2 = k2
+        self.matching_thresh = matching_threshold
 
 
     def __preprocess_video(self, video):
@@ -30,6 +32,7 @@ class KeyframeExtraction(FeatureHandler):
         preprocessed_frames =  self.__preprocess_video(video)
         hist_coeffs = []
         keyframes = []
+        video_features = []
 
         prev_hist = cv2.calcHist([preprocessed_frames[0]], [0], mask=None, histSize=[256], ranges=[0, 256])
 
@@ -40,21 +43,20 @@ class KeyframeExtraction(FeatureHandler):
             sum_of_diff = np.sum(diff)
             hist_coeffs.append(sum_of_diff)
 
-            print(f"Sum of diff: {sum_of_diff}")
+            # print(f"Sum of diff: {sum_of_diff}")
             prev_hist = hist_i
 
         mu_ = np.mean(hist_coeffs)
         std_ = np.std(hist_coeffs)
 
-        print(f"mean: {mu_}, std: {std_}")
+        # print(f"mean: {mu_}, std: {std_}")
         self.threshold_value = self.k1 * mu_ + self.k2 * std_
-        print(self.threshold_value)
 
         for i_coeff in range(len(hist_coeffs)):
             if hist_coeffs[i_coeff] > self.threshold_value:
                 keyframes.append(preprocessed_frames[i_coeff])
 
-        print(f"keyframes len: {len(keyframes)}")
+        # print(f"keyframes len: {len(keyframes)}")
         # plt.plot(hist_0, 'r')
         # plt.plot(hist_1, 'b')
         # plt.plot(diff, 'g')
@@ -64,15 +66,41 @@ class KeyframeExtraction(FeatureHandler):
         #     cv2.imshow("WD", frame)
         #     cv2.waitKey(0)
         # cv2.destroyAllWindows()
+        for frame in keyframes:
+            hist = cv2.calcHist([frame], [0], mask=None, histSize=[256], ranges=[0, 256])
+            hist = hist.flatten()
+            video_features.append(hist)
 
-    def match(self):
-        pass
+        return video_features
+    
+    def match(self, features1, features2):
+    
+        features1 = np.array(features1)
+        features2 = np.array(features2)
+        print(f"No. of KeyFrames in Feature1: {features1.shape[0]}")
+        print(f"No. of KeyFrames in Feature2: {features2.shape[0]}")
+
+        num_of_matches = 0
+        for feat_1 in range((features1.shape[0])):
+            for feat_2 in range((features2.shape[0])):
+                hist_diff = np.mean(np.abs(features1[feat_1] - features2[feat_2]))
+                print(hist_diff)
+                if hist_diff < self.matching_thresh:
+                    num_of_matches += 1
+
+
+        print(f"num_of_matches: {num_of_matches}")
+        matching_percent = num_of_matches / (features1.shape[0] * features2.shape[0])
+        print(f"Matching Percentage: {matching_percent*100}%")
+        return matching_percent
 
 
 
 
-kf = KeyframeExtraction()
-video_path = '/home/ayman/FOE-Linux/Graduation_Project/Stereo-3D-Detection/results/end-to-end_demo.mp4'
-video = cv2.VideoCapture(video_path)
+# kf = KeyframeExtraction()
+# video_path = '/home/ayman/FOE-Linux/Graduation_Project/Stereo-3D-Detection/results/end-to-end_demo.mp4'
+# video = cv2.VideoCapture(video_path)
 
-kf.extract(video)
+# feat_1 = kf.extract(video)
+# feat_2 = kf.extract(cv2.VideoCapture('/home/ayman/Videos/Screencast 2021-02-01 07:41:07.mp4'))
+# kf.match(feat_1, feat_2)
